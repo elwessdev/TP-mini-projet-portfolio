4f5de3f19612bc019247af37bcd1c2d6b598e1b1
devosema <benaliosama3@gmail.com>
2024-10-27 23:44:08 +0100

    update

--- public/config.js
+++ public/config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { CONFIG } from "./env.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD6m5_52_dnSFNM2i8yyZ1z_InasbKbZLM",
-  authDomain: "git-portfolio-d8e13.firebaseapp.com",
-  projectId: "git-portfolio-d8e13",
+  apiKey: CONFIG.API_KEY,
+  authDomain: CONFIG.AUTH_DOMAIN,
+  projectId: CONFIG.PROJECT_ID,
   storageBucket: "git-portfolio-d8e13.appspot.com",
   messagingSenderId: "1082961694136",
   appId: "1:1082961694136:web:c208e3d16bf50673fbd34d",
